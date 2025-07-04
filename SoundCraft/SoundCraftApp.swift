1a8149871b6614ce54ab3098e28dcf32ef46f52a
Abe M <abe.malla8@gmail.com>
2025-07-04 16:21:16 -0700

    Initial Commit

---
+++ SoundCraft/SoundCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundCraftApp.swift
+//  SoundCraft
+//
+//  Created by Abe Malla on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
